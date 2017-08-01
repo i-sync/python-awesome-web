e22943a68baf62e700097cabaf1df240e49c7386
Michael.Tian <michael.tian@loweprofero.com>
2017-08-01 14:29:55 +0800

    finish day 08.

--- www/handlers.py
+++ www/handlers.py
@@ -21,6 +21,20 @@ def index(request):
         'users': users
     }
 
+@get('/blogs')
+def blogs(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test blog', summary = summary, created_at = time.time()-120),
+        Blog(id='2', name='Javascript IT', summary = summary, created_at = time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary = summary, created_at = time.time()-7200),
+    ]
+
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
+
 @get('/test')
 def test(request):
     return web.Response(body=b'<h1>Awesome Python3 Web</h1>', content_type='text/html')
